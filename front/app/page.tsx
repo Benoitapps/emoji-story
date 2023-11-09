4c23a82fe52710deaa3e4d9e7d9fd6203b403978
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-11-09 10:31:38 +0100

    connected to back

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Step from "@/component/UI/step/Step";
 import { useEffect, useState } from "react";
-import { StoryStep } from "interface/emoji";
+import { Story, StoryStep } from "interface/emoji";
 import { ClientToServerEvent, ServerToClientEvent } from "interface/event";
 import { Socket, io } from "socket.io-client";
 
@@ -9,81 +9,29 @@ const socket: Socket<ServerToClientEvent, ClientToServerEvent> = io(
   process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000"
 );
 
-const primaryStep: StoryStep = {
-  selectedEmoji: "",
-  emojiContender: [
-    {
-      value: "🚶🏼",
-      votes: 0,
-    },
-    {
-      value: "🏊🏻",
-      votes: 0,
-    },
-    {
-      value: "💅🏿",
-      votes: 0,
-    },
-    {
-      value: "🍍",
-      votes: 0,
-    },
-    {
-      value: "👯",
-      votes: 0,
-    },
-    {
-      value: "📡",
-      votes: 0,
-    },
-    {
-      value: "👧🏽",
-      votes: 0,
-    },
-    {
-      value: "🙋🏾",
-      votes: 0,
-    },
-  ],
-};
-
 export default function Home() {
-  const [step, setStep] = useState<StoryStep>(primaryStep);
+  const [story, setStory] = useState<Story>();
   const [timeLeft, setTimeLeft] = useState<number>(20);
 
   useEffect(() => {
     socket.on("story-update", (data) => {
-      console.log({ data });
+      setStory(data);
     });
   }, []);
 
-  const handleVote = (emoji: string) => {
-    setStep((s) => {
-      if (!s.emojiContender) return s;
-      const index = s.emojiContender?.findIndex((e) => e.value === emoji);
+  const step0 = story?.steps?.[0]
 
-      if (index === -1 || !index) {
-        return s;
-      }
-
-      const current = s.emojiContender[index];
-
-      s.emojiContender?.splice(index, 1, {
-        ...current,
-        votes: current.votes + 1,
-      });
-
-      return s;
-    });
-  };
+  const handleVote = (emoji: string) => {};
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Step
-        step={step}
-        stepNumber={0}
-        timeLeft={timeLeft}
-        handleEmojiClick={handleVote}
-      />
+      {step0 && (
+        <Step
+          step={step0}
+          stepNumber={0}
+          timeLeft={timeLeft}
+          handleEmojiClick={handleVote}
+        />
+      )}
     </main>
   );
 }
